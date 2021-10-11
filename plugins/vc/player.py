912392237c0a15fd30ffbf673767c531f27f5055
Puutraaa <88121146+Puutraaa@users.noreply.github.com>
2021-10-11 11:36:18 +0800

    Update player.py

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -78,7 +78,7 @@ __starts with ! (exclamation mark)__
 
 USERBOT_REPO = f"""{emoji.ROBOT} **BOT BATTLE DESAH**
 
-- Repository: [github](https://github.com/Puutraaa/Desah-userbot)
+- Repository: [github](https://github.com/Puutraaa/Desah-bot)
 - License: AGPL-3.0-or-later"""
 
 # - Pyrogram filters
